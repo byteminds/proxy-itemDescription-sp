36f23e2b88193f584e145e2042f0ceb55e657702
sparksparker <sehyunspark@gmail.com>
2019-08-06 22:51:29 -0700

    Update w/ new link for photo gallery/item description microservice && comment out reviews microservice due to it not currently running

--- proxy/server.js
+++ proxy/server.js
@@ -15,14 +15,16 @@ app.use(express.static('public'));
 
 app.use('/:id', express.static(path.join(__dirname, 'public')));
 
+// picture gallery and item description microservice
 app.get('/descriptions/:id', (req, res) => {
   const id = req.params.id;
-  const url = `http://ec2-54-67-53-174.us-west-1.compute.amazonaws.com/descriptions/${id}`;
+  const url = `http://ec2-54-193-122-43.us-west-1.compute.amazonaws.com/descriptions/${id}`;
   axios.get(url)
     .then(data => res.send(data.data))
     .catch(error => console.log('ERROR', error))
 });
 
+// checkout microservice
 app.get('/api/:id', (req, res) => {
   const id = req.params.id;
   const url = `http://ec2-3-17-206-111.us-east-2.compute.amazonaws.com/pricingAPI/${id}`;
@@ -31,13 +33,14 @@ app.get('/api/:id', (req, res) => {
     .catch(error => console.log('ERROR', error))
 });
 
-app.get('/reviews/api/:productId', (req, res) => {
-  const id = req.params.id;
-  const url = `http://ec2-34-201-59-177.compute-1.amazonaws.com/reviews/api/${id}`;
-  axios.get(url)
-    .then(data => res.send(data.data))
-    .catch(error => console.log('ERROR', error))
-});
+// reviews microservice is not running atm
+// app.get('/reviews/api/:productId', (req, res) => {
+//   const id = req.params.id;
+//   const url = `http://ec2-34-201-59-177.compute-1.amazonaws.com/reviews/api/${id}`;
+//   axios.get(url)
+//     .then(data => res.send(data.data))
+//     .catch(error => console.log('ERROR', error))
+// });
 
 app.listen(
   port,
